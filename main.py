8e0d77dd2e0b9f2e19c80241caee52be9541302c
arturbednarz <slliver@icloud.com>
2024-03-20 13:53:45 +0100

    Create base template

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import os
+from os.path import join, dirname
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
+Bootstrap5(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True, port=5050)
