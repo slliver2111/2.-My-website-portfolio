9c28c81259226db432870fe86f3b10387ebd949f
arturbednarz <slliver@icloud.com>
2024-03-28 20:48:03 +0100

    Activate form to contact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
+import smtplib
 from os.path import join, dirname
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 from flask_bootstrap import Bootstrap5
 from dotenv import load_dotenv
 
@@ -11,10 +12,29 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 Bootstrap5(app)
 
+EMAIL_LOGIN = os.environ.get("EMAIL_LOGIN")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
-@app.route('/')
+
+@app.route("/", methods=["POST", "GET"])
 def home():
+    if request.method == "POST":
+        data = request.form
+        try:
+            send_email(data["name"], data["email"], data["phone"], data["message"])
+            flash('Message successfully sent.')
+        except:
+            flash('There was an error in sending message.')
+        return redirect("#contact")
     return render_template('index.html')
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL_LOGIN, EMAIL_PASSWORD)
+        connection.sendmail(EMAIL_LOGIN, 'slliver@icloud.com', email_message)
 
 
 if __name__ == '__main__':
